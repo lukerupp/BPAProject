4b6435b05455f7057f7641d3352d1d287408deab
cdubles <33189090+cdubles@users.noreply.github.com>
2021-01-12 15:35:54 -0500

    add Db file and start outline

--- mainServer.js
+++ mainServer.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const SQL  = require('sqlite3')
+const SQL = require('sqlite3').verbose();
 
+let db = new SQL.Database('./database/database.db');
 const PORT = 8080;
 
 app.use(express.static('views'))
 server.listen(PORT)
+
+//login standard
+app.post('login', function() {
+
+})
+
+//
+//signup as standard
+app.post('signup', function() {
+
+})
